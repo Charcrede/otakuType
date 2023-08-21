bdcebacb6800e0bffa5050bfdd78aa9942e535f0
Charcrede <charcrede@gmail.com>
2023-08-21 18:25:28 +0100

    sdfsf

--- src/app/routing-module/routing-module.module.ts
+++ src/app/routing-module/routing-module.module.ts
@@ -1,12 +1,46 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AcceuilComponent } from '../acceuil/acceuil.component';
+import { CitationsComponent } from '../citations/citations.component';
+import { PlayCitComponent } from '../play-cit/play-cit.component';
+import { PlaySynopComponent } from '../play-synop/play-synop.component';
+import { SynopsisComponent } from '../synopsis/synopsis.component';
+import { RouterModule, Routes } from '@angular/router';
+const typeRoute: Routes = [
+  {
+    path : 'acceuil',
+    component: AcceuilComponent,
+  },
+  {
+    path : 'citations',
+    component: CitationsComponent,
+  },
+  {
+    path : 'citation-play',
+    component: PlayCitComponent,
+  },
+  {
+    path : 'synopsis',
+    component: SynopsisComponent,
+  },
+  {
+    path : 'synopsis-play',
+    component: PlaySynopComponent,
+  },
+  {
+    path : '',
+    component: AcceuilComponent,
+  },
+]
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forRoot(typeRoute),
+  ],
+  exports:[RouterModule],
 })
 export class RoutingModuleModule { }
--- src/app/type-service.service.ts
+++ src/app/type-service.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { SYNOPSIS } from './mockOtaku_second';
+import { Synopsis } from './otaku';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,19 @@ import { Injectable } from '@angular/core';
 export class TypeServiceService {
 
   constructor() { }
+  Synopsys: Synopsis[] = SYNOPSIS;
+  selectedSynopsis!:Synopsis;
+  words!: string[][];
+  image!: string;
+  getSynopsis(synop: Synopsis){
+    this.selectedSynopsis = synop;
+    let tab1 = this.selectedSynopsis.texte.split(" ");
+    let tab2 = [];
+    for (let i = 0; i < tab1.length; i++) {
+      tab2.push(tab1[i].split(""));
+    }
+    this.words = tab2;
+    this.image = this.selectedSynopsis.url;
+  }
+
 }
