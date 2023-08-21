529e181c60a35dc6853c8111e3e213ab4966c029
Euloge <eagnoun@gmail.com>
2023-08-21 10:50:21 +0100

    euloge

--- src/app/mockOtaku.ts
+++ src/app/mockOtaku.ts
@@ -10,9 +10,37 @@ export const CITATIONS: Citations[] = [
     },
     {
         "id" : "2",
-        "persoName" : "Uzumaki Naruto",
-        "animeName" : "Naruto",
-        "url" : "../assets/citations/img/2.png", 
-        "text" : "Ne vivez pas avec des mensonges ou des peurs, car vous finirez par vous détester"
+        "persoName" : "Roronoa Zoro",
+        "animeName" : "One Piece",
+        "url" : "../assets/citations/img/3.png", 
+        "text" : "Peu m'importe que les dieux existent ou non, je suis le maître de ma vie. étape des questions religieuses, mais ne méprisez jamais quelqu'un qui y croit"
+    },
+    {
+        "id" : "3",
+        "persoName" : "Hitsugaya Toushiro",
+        "animeName" : "Bleach",
+        "url" : "../assets/citations/img/4.png", 
+        "text" : "Le ciel n'est-il pas incroyable? Peu importe le nombre de fois que vous le regardez, ce n'est jamais la même chose deux fois. Ce paradis de maintenant n'existe qu'en ce moment"
+    },
+    {
+        "id" : "4",
+        "persoName" : "Edward Elric",
+        "animeName" : "Fullmetal Alchemist Brotherhood",
+        "url" : "../assets/citations/img/5.png", 
+        "text" : "Levez-vous et avancez, vous avez vos jambes pour le faire"
+    },
+    {
+        "id" : "5",
+        "persoName" : "L.",
+        "animeName" : "Death Note",
+        "url" : "../assets/citations/img/6.png", 
+        "text" : "Peu importe votre qualification, vous seul ne pouvez pas changer le monde. Mais c'est la beauté du monde"
+    },
+    {
+        "id" : "6",
+        "persoName" : "Spike",
+        "animeName" : "Cowboy Bepop",
+        "url" : "../assets/citations/img/7.png", 
+        "text" : " Il y avait une femme, c'était la première fois que je rencontrais quelqu'un qui était vraiment en vie. Au moins, c'est ce que je pensais. Elle était… la partie de moi que j'ai perdue quelque part en cours de route, la partie qui manquait, la partie que je voulais."
     },
 ];
