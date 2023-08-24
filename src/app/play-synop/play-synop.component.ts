4f27808935ccf5678306fa38c9235e587f298594
Charcrede <charcrede@gmail.com>
2023-08-24 10:25:27 +0100

    df

--- src/app/play-synop/play-synop.component.ts
+++ src/app/play-synop/play-synop.component.ts
@@ -2,7 +2,7 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { TypeServiceService } from '../type-service.service';
 import { NgModel, NgForm, ControlContainer } from '@angular/forms';
 import { keyframes } from '@angular/animations';
-import { fromEvent, Observable } from 'rxjs';
+import { fromEvent, Observable, TimeoutConfig } from 'rxjs';
 
 @Component({
   selector: 'app-play-synop',
@@ -13,33 +13,43 @@ export class PlaySynopComponent implements OnInit {
   sentence!: string[];
   entered!: string;
   i: number = 0;
+  count: number = 0;
   u: number = 0;
   errorsCount: number = 0;
   spans!: any;
   lose: boolean = false;
   success: boolean = false;
   textInput!: HTMLElement | null;
-  userKeydown!:Observable<InputEvent>;
-  subscription!:any;
+  userKeydown!: Observable<InputEvent>;
+  subscription!: any;
+  speed: number = 0;
+  time: string = "00:00";
+  precision: number = 0;
+  typeCount = 0;
+  intervalId!: any;
   selectedSynopsis = this.service.selectedSynopsis;
   constructor(public service: TypeServiceService) { }
   ngOnInit(): void {
     setTimeout(() => {
       this.spans = document.querySelectorAll(".lettre");
-        this.typing();
+      this.typing();
     }, 100);
   }
   @ViewChild('texte') texte !: ElementRef;
-    typing() {
-      const container = this.texte.nativeElement;
-      this.textInput = document.getElementById("input");
-      if (this.textInput && this.selectedSynopsis) {
-        this.userKeydown = fromEvent<InputEvent>(this.textInput, 'input');
+  typing() {
+    this.typeCount++
+    if (this.typeCount === 2) {
+      this.timing();
+    }
+    const container = this.texte.nativeElement;
+    this.textInput = document.getElementById("input");
+    if (this.textInput && this.selectedSynopsis) {
+      this.userKeydown = fromEvent<InputEvent>(this.textInput, 'input');
       this.sentence = this.selectedSynopsis.texte.split("");
     }
     this.subscription = this.userKeydown.subscribe((e) => {
       let newTabSentence = this.entered.split("");
-      if (e.inputType ==='insertText') {
+      if (e.inputType === 'insertText') {
         if (this.i <= this.sentence.length) {
           if (this.sentence[this.i] === newTabSentence[this.i]) {
             this.spans[this.i].classList.add("success")
@@ -49,7 +59,7 @@ export class PlaySynopComponent implements OnInit {
             this.spans[this.i].classList.remove("success")
             this.errorsCount++;
           }
-          
+
           if (this.u > 0) {
             this.spans[this.u].classList.remove("current");
           }
@@ -57,8 +67,8 @@ export class PlaySynopComponent implements OnInit {
           this.u++;
           
         }
-        if ((this.spans[this.i].offsetTop+((this.spans[this.i].offsetHeight*2)) > container.offsetHeight) && this.spans[this.i].offsetTop > this.spans[this.i-1].offsetTop) {
-          container.scrollTop = container.scrollTop+this.spans[this.i].offsetHeight+25.75;
+        if ((this.spans[this.i].offsetTop + ((this.spans[this.i].offsetHeight * 2)) > container.offsetHeight) && this.spans[this.i].offsetTop > this.spans[this.i - 1].offsetTop) {
+          container.scrollTop = container.scrollTop + this.spans[this.i].offsetHeight + 25.75;
         }
         this.spans[this.u].classList.add("current");
       }
@@ -72,7 +82,27 @@ export class PlaySynopComponent implements OnInit {
           this.spans[this.i].classList.remove("lose");
         }
       }
+      let sensLettersLength = this.selectedSynopsis.texte.split("").length;
+      let entersWordsLength = this.entered.split("").length;
+      this.speed = Math.ceil(entersWordsLength / ((this.count - (this.errorsCount * 0.5))));
+      this.precision = Math.floor(((sensLettersLength - this.errorsCount) / sensLettersLength) * 100);
       this.subscription.unsubscribe();
     })
+  }
+  timing() {
+    let min: number;
+    let sec: number;
+    this.intervalId = setInterval(() => {
+      this.count++;
+      sec = this.count - min * 60;
+      min = Math.floor(this.count / 60);
+      this.time = `${min}:${sec}`
+      if (this.i === this.selectedSynopsis.texte.length-1) {
+        clearInterval(this.intervalId)
+        // score.style.display = "block";
+        // pourcent.innerText = `${precis}%`
+        // errors.innerText = `${this.errorsCount}`;
+      }
+    }, 1000);
   }
 }
