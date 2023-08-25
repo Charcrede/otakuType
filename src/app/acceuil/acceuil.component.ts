3e0597cd4ded5cdacf33143379d75d031f79956f
Euloge <eagnoun@gmail.com>
2023-08-25 15:54:18 +0100

    euloge

--- src/app/acceuil/acceuil.component.ts
+++ src/app/acceuil/acceuil.component.ts
@@ -10,31 +10,34 @@ export class AcceuilComponent implements OnInit {
   opacities: number[] = [];
   inputText!: string ;
   space !: string ;
+  letters : string[][] = [];
+  letter !: string[];
+  finalLetter !: string;
+  opacities1: number[] = [];
 
   ngOnInit() {
     this.inputText = "DES SYNOPSIS ET CITATIONS MANGA POUR TESTER VOTRE VITESSE DE SAISIE"; // Remplacez par votre chaîne de caractères
     // this.startAnimation();
     this.words = this.inputText.split(' ');
+    for (let i = 0; i < this.words.length; i++) {
+      this.letters.push(this.words[i].split(""))
+    }
     this.opacities = this.words.map(() => 0);
+    this.opacities1 = this.words.map(() => 0);
+   
 
-    this.words.forEach((word, index) => {
+    this.words.forEach((letters, index) => {
       setTimeout(() => {
         this.opacities[index] = 1;
-        this.space = " ";
-      }, index * 700);
+      }, index * 600);
     });
-  }
 
-}
-writter(sentences: string){
-  let tab  = sentences.split("");
-  let newTab:string[] = [];
-  let i = 0;
-  setInterval(() => {
-    if (i < tab.length){
-      newTab.push(tab[i]);
-      this.newSentences = newTab.join("");
-      i++;
-    }
-  }, 70);
+    this.letters.forEach((letter, i)=>{
+      setTimeout(() => {
+        this.opacities1[i] = 1;
+      }, 600);
+    })
+
+   
+  }
 }
